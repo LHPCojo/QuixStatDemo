983d2aa07e6d0a5d7509a82d51a8642f5c6f7cb5
Michael Cojocari <michael.cojocari@lhpes.com>
2023-10-31 20:24:50 +0000

    Edited 'main.py'

--- CoinAPI/main.py
+++ CoinAPI/main.py
@@ -58,7 +58,8 @@ try:
             # We sleep for 15 minutes so we don't reach free COIN API account limit.
             # Stop sleeping if process termination requested
             sleeping = 0
-            while sleeping <= 5 and run:
+            sleep_wake = 1
+            while sleeping <= sleep_wake and run:
                 sleeping = sleeping + 1
                 time.sleep(1)
 
