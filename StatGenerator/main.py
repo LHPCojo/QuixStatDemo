3c2bf7b81953dff303f3e85b76ba0e240db1be23
Michael Cojocari <michael.cojocari@lhpes.com>
2023-11-02 19:24:09 +0000

    Edited 'main.py'

--- StatGenerator/main.py
+++ StatGenerator/main.py
@@ -20,7 +20,6 @@ stream.properties.name = "StatGenerator Stream"
 
 # Add metadata about time series data you are about to send. 
 
-stream.timeseries.add_definition("label")
 for topic in topics:
     stream.timeseries.add_definition(topic["topic_name"]).set_range(topic["topic_minimum"], topic["topic_maximum"])
     stream.timeseries.buffer.time_span_in_milliseconds = 100
@@ -31,9 +30,6 @@ print("Sending values for " + str(totalGeneratorTime / 100) + " seconds.")
 
 for index in range(0, totalGeneratorTime):
     utc_now = datetime.datetime.utcnow()
-    
-    stream.timeseries.buffer.add_timestamp(utc_now).add_value("label", "HELLO")
-
     for topic in topics:
         stream.timeseries.buffer \
             .add_timestamp(utc_now) \
