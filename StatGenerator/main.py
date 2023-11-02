a01a6d0112568b8cfb2e6a0b9fcbca4761d8dec4
Michael Cojocari <michael.cojocari@lhpes.com>
2023-11-02 18:28:31 +0000

    Edited 'main.py'

--- StatGenerator/main.py
+++ StatGenerator/main.py
@@ -16,29 +16,29 @@ for topic in topics:
     print(topic["topic_name"])
 
 
-# # Open the output topic where to write data out
-# topic_producer = client.get_topic_producer(topic_id_or_name = os.environ["output"])
+# Open the output topic where to write data out
+topic_producer = client.get_topic_producer(topic_id_or_name = os.environ["output"])
 
-# # Set stream ID or leave parameters empty to get stream ID generated.
-# stream = topic_producer.create_stream()
-# stream.properties.name = "StatGenerator Stream"
+# Set stream ID or leave parameters empty to get stream ID generated.
+stream = topic_producer.create_stream()
+stream.properties.name = "StatGenerator Stream"
 
-# # Add metadata about time series data you are about to send. 
-# stream.timeseries.add_definition(topic_name).set_range(-1.2, 1.2)
-# stream.timeseries.buffer.time_span_in_milliseconds = 100
+# Add metadata about time series data you are about to send. 
+stream.timeseries.add_definition(topic_name).set_range(-1.2, 1.2)
+stream.timeseries.buffer.time_span_in_milliseconds = 100
 
-# totalGeneratorTime = 30000;
-# print("Sending values for " + str(totalGeneratorTime / 100) + " seconds.")
+totalGeneratorTime = 30000;
+print("Sending values for " + str(totalGeneratorTime / 100) + " seconds.")
 
-# for index in range(0, totalGeneratorTime):
-#             # .add_value("ParameterA", math.sin(index / 200.0) + math.sin(index) / 5.0) \
+for index in range(0, totalGeneratorTime):
+            # .add_value("ParameterA", math.sin(index / 200.0) + math.sin(index) / 5.0) \
 
-#     stream.timeseries \
-#         .buffer \
-#         .add_timestamp(datetime.datetime.utcnow()) \
-#         .add_value(topic_name, 1) \
-#         .publish()
-#     time.sleep(0.01)
+    stream.timeseries \
+        .buffer \
+        .add_timestamp(datetime.datetime.utcnow()) \
+        .add_value(topic_name, 1) \
+        .publish()
+    time.sleep(0.01)
 
-# print("Closing stream")
-# stream.close()
+print("Closing stream")
+stream.close()
